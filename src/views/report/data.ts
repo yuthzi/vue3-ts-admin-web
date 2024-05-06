8089ef9bf7b9f10a77f89fad573e095098a3bacd
yuying_xu <yuth@163>
2024-05-06 16:39:09 +0800

    封装占比环形图组件

---
+++ src/views/report/data.ts
@@ -0,0 +1,33 @@
+/* 初始化 charts参数 */
+export const ageData = [
+  {
+    value: 200,
+    name: '10元以下',
+    percentage: '16%',
+  },
+  {
+    value: 110,
+    name: '10 - 18元',
+    percentage: '8%',
+  },
+  {
+    value: 150,
+    name: '18 - 30元',
+    percentage: '12%',
+  },
+  {
+    value: 310,
+    name: '30 - 40元',
+    percentage: '24%',
+  },
+  {
+    value: 250,
+    name: '40 - 60元',
+    percentage: '20%',
+  },
+  {
+    value: 260,
+    name: '60元以上',
+    percentage: '20%',
+  },
+]
--- src/views/report/order/index.vue
+++ src/views/report/order/index.vue
@@ -1,73 +1,48 @@
 <template>
-  <v-chart class="chart card" :option="option" autoresize />
+  <div class="card">
+    <div class="chart-title">今天订单金额占比分析</div>
+    <div class="chart">
+      <!-- chart区域 -->
+      <RatioPieChart ref="ratioPieChartRef" />
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { use } from 'echarts/core'
-import { CanvasRenderer } from 'echarts/renderers'
-import { PieChart } from 'echarts/charts'
-import 'echarts/theme/shine.js'
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import type { ECharts } from 'echarts'
+import { ageData } from '@/views/report/data'
+import RatioPieChart from '@/views/report/order/components/RatioPieChart.vue'
 
-import {
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-} from 'echarts/components'
-import VChart, { THEME_KEY } from 'vue-echarts'
-import { ref, provide } from 'vue'
-
-use([
-  CanvasRenderer,
-  PieChart,
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-])
-
-provide(THEME_KEY, 'shine')
-
-const option = ref({
-  title: {
-    text: 'Traffic Sources',
-    left: 'center',
-  },
-  tooltip: {
-    trigger: 'item',
-    formatter: '{a} <br/>{b} : {c} ({d}%)',
-  },
-  legend: {
-    orient: 'vertical',
-    left: 'left',
-    data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
-  },
-  series: [
-    {
-      name: 'Traffic Sources',
-      type: 'pie',
-      radius: '55%',
-      center: ['50%', '60%'],
-      data: [
-        { value: 335, name: 'Direct' },
-        { value: 310, name: 'Email' },
-        { value: 234, name: 'Ad Networks' },
-        { value: 135, name: 'Video Ads' },
-        { value: 1548, name: 'Search Engines' },
-      ],
-      emphasis: {
-        itemStyle: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 0, 0, 0.5)',
-        },
-      },
-    },
-  ],
+/* 获取最外层盒子 */
+onMounted(() => {
+  initCharts()
 })
+
+/* 声明echarts实例 */
+interface ChartProps {
+  [key: string]: ECharts | null
+}
+const dataScreen: ChartProps = {
+  chart: null,
+}
+
+/* 获取子组件的ref */
+interface ChartExpose {
+  initChart: (params: any) => ECharts
+}
+
+const ratioPieChartRef = ref<ChartExpose>()
+
+/* 初始化 echarts */
+const initCharts = (): void => {
+  dataScreen.chart = ratioPieChartRef.value?.initChart(ageData) as ECharts
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
 .chart {
   width: 500px;
-  height: 500px;
+  height: 300px;
 }
 </style>
