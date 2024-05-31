561eab3c5e1efbf8d433731b15e214bd2f7222e5
yuthzi <yuth89@163.com>
2024-05-31 10:31:23 +0800

    完善父子组件传值demo

--- src/views/dev/shareData/components/Middle.vue
+++ src/views/dev/shareData/components/Middle.vue
@@ -1,23 +1,15 @@
 <template>
   <div>
-    <p>第2层组件: {{ searchParam[key] }}</p>
-    <Grandson v-model="searchParam[key]" />
+    <p>第2层组件: {{ sharedData!.title }}</p>
+    <Grandson :sharedData="sharedData" />
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import Grandson from './Grandson.vue'
 import { defineProps } from 'vue'
 
-const key = 'title'
-
-interface SearchFormItem {
-  searchParam: { [key: string]: any }
-}
-
-const props = defineProps<SearchFormItem>()
-
-// defineProps({
-//   sharedData: Object,
-// })
+defineProps({
+  sharedData: Object,
+})
 </script>
--- src/views/dev/shareData/index.vue
+++ src/views/dev/shareData/index.vue
@@ -4,8 +4,7 @@
       使用reactive实现父子组件双向数据绑定。该例子有3层组件，最底层的组件输入值，显示在每一层组件
     </p>
     <p>第1层组件: {{ sharedData.title }}</p>
-    <!-- <Grandson :sharedData="sharedData" /> -->
-    <Middle :searchParam="sharedData" />
+    <Middle :sharedData="sharedData" />
   </div>
 </template>
 
@@ -14,12 +13,7 @@
  * 使用reactive实现父子组件双向数据绑定。该例子
  */
 import { reactive } from 'vue'
-// import Grandson from './components/Grandson.vue'
 import Middle from './components/Middle.vue'
-
-// interface ShareProps {
-//   title: string;
-// }
 
 const sharedData = reactive({
   title: 'a',
