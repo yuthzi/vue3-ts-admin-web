6a8c1c23046415cb440de3e0e8568d56f90c217a
yuthzi <yuth89@163.com>
2024-06-01 20:55:30 +0800

    顾客列表添加状态搜索

--- src/views/customer/level/index.vue
+++ src/views/customer/level/index.vue
@@ -87,6 +87,17 @@ const columns: ColumnProps[] = [
     onChange: onChangeStatus,
     activeValue: 1,
     inactiveValue: 0,
+    enum: [
+      {
+        value: 0,
+        label: '禁用',
+      },
+      {
+        value: 1,
+        label: '启用',
+      },
+    ],
+    search: { el: 'select', key: 'status' },
   },
   { prop: 'gmtCreate', label: '创建时间', sortable: true },
   { prop: 'gmtModified', label: '更新时间', sortable: true },
