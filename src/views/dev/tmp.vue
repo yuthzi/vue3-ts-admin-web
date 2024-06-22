674a94ef6dffbe0ed484c229458fc73cf7a1f5ef
yuthzi <yuth89@163.com>
2024-06-22 21:48:18 +0800

    打开新页签例子

--- src/views/dev/tmp.vue
+++ src/views/dev/tmp.vue
@@ -1,92 +1,20 @@
 <template>
   <div>
-    <CardList
-      ref="cardList"
-      :requestApi="getJobList"
-      :dataCallback="dataCallback"
-      :pageSize="20"
-      :leftTop="leftTop"
-      :rightTop="rightTop"
-      :center="center"
-      :leftBottom="leftBottom"
-    ></CardList>
+    <el-button @click="handleOpenCardList">打开新页签</el-button>
   </div>
 </template>
 
 <script lang="tsx" setup>
-import { ref } from 'vue'
-import { getJobList } from '@/api/job/list/api'
-import { Job } from '@/api/job/list/type'
-// import { getBrandList } from '@/api/goods/brand/api'
-import { dataCallback } from '@/utils/pagination'
-import { CardAreaProps } from '@/components/CardList/src/types'
+import { useRouter } from 'vue-router'
 
-const leftTop: CardAreaProps<Job.ResJobList> = {
-  prop: 'title',
-  // color: 'red',
-  fontSize: '16px',
-  render: ({ row }) => {
-    let title = row.title
-    if (row.location) {
-      return (
-        <>
-          <span>{title} </span>
-          <span class="left-top-right">[{row.location}] </span>
-        </>
-      )
-    } else {
-      return <>{title}</>
-    }
-  },
+const router = useRouter()
+
+const handleOpenCardList = () => {
+  // openCardList()
+  console.log('router=' + router)
+  // router.push('/view/jobList')
+  router.push('/card/list')
 }
-
-const rightTop: CardAreaProps<Job.ResJobList> = {
-  prop: 'companyName',
-  // color: 'red',
-  fontSize: '16px',
-}
-
-const center: CardAreaProps<Job.ResJobList> = {
-  prop: 'salary',
-  color: 'red',
-  fontSize: '16px',
-}
-
-const leftBottom: CardAreaProps<Job.ResJobList> = {
-  prop: 'tag',
-  // color: 'red',
-  fontSize: '16px',
-  render: ({ row }) => {
-    // console.log(JSON.stringify(row))
-    let cols = row.tag?.split(',')
-    if (cols) {
-      // 只展示前5
-      if (cols.length > 5) {
-        cols = cols.slice(0, 5)
-      }
-
-      return (
-        <>
-          {cols.map((item) => (
-            <el-tag class="bottom-tag">{item}</el-tag>
-          ))}
-        </>
-      )
-    } else {
-      return ''
-    }
-  },
-}
-
-const cardList = ref()
 </script>
 
-<style lang="scss" scoped>
-:deep .bottom-tag {
-  margin-right: 10px;
-}
-
-:deep .left-top-right {
-  margin-left: 12px;
-}
-</style>
+<style lang="scss" scoped></style>
